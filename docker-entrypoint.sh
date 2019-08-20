dc97853b13d2febfb4421cf8369cd05028497928
Artur <anialpro@gmail.com>
2019-08-20 19:15:00 +0400

    add docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [[ -n "${RECIPIENT}" ]]; then
+    # add domain for receive mails
+    RECIPIENT_DOMAIN="$(echo "$RECIPIENT" | cut -d "@" -f 2)"
+    postconf mydestination="\$myhostname, localhost, ${RECIPIENT_DOMAIN}"
+
+    # create "blackhole" for bounce message
+    echo "${RECIPIENT} discard:silently" > /etc/postfix/transport \
+    && postconf -e transport_maps="hash:/etc/postfix/transport" \
+    && postmap /etc/postfix/transport
+fi
+
+exec "$@"
